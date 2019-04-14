fb03d5862d9a0bc11c7e4918a102b0afa660c4b8
joesdesk <jomar.sastrillo@gmail.com>
2019-04-14 04:58:18 -0600

    Configure dependencies, cors and database

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rest-api-template
 Rest API Template in Django
+
+## Dependencies
+
+```
+conda install -c conda-forge djangorestframework
+conda install -c conda-forge django-cors-headers
+```
--- apitemplate/settings.py
+++ apitemplate/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+	'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'apitemplate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database.db'),
     }
 }
 
